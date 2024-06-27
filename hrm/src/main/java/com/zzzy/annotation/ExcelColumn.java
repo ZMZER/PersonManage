29daa639f51067384f29f1d3de65ce3e204118ff
zmer <1632758201@qq.com>
2024-06-27 13:25:55 +0800

    1

--- hrm/src/main/java/com/zzzy/annotation/ExcelColumn.java
+++ hrm/src/main/java/com/zzzy/annotation/ExcelColumn.java
@@ -6,12 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @Author qiujie
- * @Date 2022/3/29
- * @Version 1.0
- */
-
-/**
  * 声明该注解类的生命周期
  * RUNTIME表示注解保留在程序运行期间，此时可以通过反射获得定义在某个类上的所有注解。
  */
